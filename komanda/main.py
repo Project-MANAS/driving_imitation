1d51b63464f6b2817434304da986ea5a4fc77ef5
Reuben John <reubenvjohn@gmail.com>
2017-08-11 23:50:21 +0530

    Shifted modules into name scopes.

--- komanda/main.py
+++ komanda/main.py
@@ -15,11 +15,12 @@ with tf.device('/gpu:0'):
 
 
 def get_optimizer(loss, lrate):
-	optimizer = tf.train.AdamOptimizer(learning_rate=lrate)
-	gradvars = optimizer.compute_gradients(loss)
-	gradients, v = zip(*gradvars)
-	gradients, _ = tf.clip_by_global_norm(gradients, 15.0)
-	return optimizer.apply_gradients(zip(gradients, v))
+	with tf.name_scope("optimizer"):
+		optimizer = tf.train.AdamOptimizer(learning_rate=lrate)
+		gradvars = optimizer.compute_gradients(loss)
+		gradients, v = zip(*gradvars)
+		gradients, _ = tf.clip_by_global_norm(gradients, 15.0)
+		return optimizer.apply_gradients(zip(gradients, v))
 
 
 def apply_vision_simple(video, keep_prob, batch_size, seq_len, scope=None, reuse=None):
@@ -91,95 +92,100 @@ class SamplingRNNCell(tf.nn.rnn_cell.RNNCell):
 graph = tf.get_default_graph()
 
 with graph.as_default():
-	# inputs
-	learning_rate = tf.placeholder_with_default(input=1e-4, shape=())
-	keep_prob = tf.placeholder_with_default(input=1.0, shape=())
-	aux_cost_weight = tf.placeholder_with_default(input=0.1, shape=())
+	with tf.name_scope("model"):
+		# inputs
+		learning_rate = tf.placeholder_with_default(input=1e-4, shape=(), name="learning_rate")
+		keep_prob = tf.placeholder_with_default(input=1.0, shape=(), name="keep_prob")
+		aux_cost_weight = tf.placeholder_with_default(input=0.1, shape=(), name="aux_cost_weight")
 
-	with tf.device('/gpu:0'):
-		targets_normalized = tf.placeholder(shape=(BATCH_SIZE, SEQ_LEN, OUTPUT_DIM), dtype=tf.float32)
-		video = tf.placeholder(shape=[BATCH_SIZE, LEFT_CONTEXT + SEQ_LEN, HEIGHT, WIDTH, CHANNELS], dtype=tf.float32)
-		visual_conditions_reshaped = apply_vision_simple(video=video, keep_prob=keep_prob,
-														 batch_size=BATCH_SIZE, seq_len=SEQ_LEN)
-		visual_conditions = tf.reshape(visual_conditions_reshaped, [BATCH_SIZE, SEQ_LEN, -1])
-		visual_conditions = tf.nn.dropout(x=visual_conditions, keep_prob=keep_prob)
+		with tf.device('/gpu:0'):
+			targets_normalized = tf.placeholder(shape=(BATCH_SIZE, SEQ_LEN, OUTPUT_DIM), dtype=tf.float32)
+			video = tf.placeholder(shape=[BATCH_SIZE, LEFT_CONTEXT + SEQ_LEN, HEIGHT, WIDTH, CHANNELS],
+								   dtype=tf.float32,
+								   name="video_input")
+			visual_conditions_reshaped = apply_vision_simple(video=video, keep_prob=keep_prob,
+															 batch_size=BATCH_SIZE, seq_len=SEQ_LEN)
+			visual_conditions = tf.reshape(visual_conditions_reshaped, [BATCH_SIZE, SEQ_LEN, -1])
+			visual_conditions = tf.nn.dropout(x=visual_conditions, keep_prob=keep_prob)
 
-		rnn_inputs_with_ground_truth = (visual_conditions, targets_normalized)
-		rnn_inputs_autoregressive = (
-			visual_conditions, tf.zeros(shape=(BATCH_SIZE, SEQ_LEN, OUTPUT_DIM), dtype=tf.float32))
+			rnn_inputs_with_ground_truth = (visual_conditions, targets_normalized)
+			rnn_inputs_autoregressive = (
+				visual_conditions, tf.zeros(shape=(BATCH_SIZE, SEQ_LEN, OUTPUT_DIM), dtype=tf.float32))
 
-		internal_cell = tf.nn.rnn_cell.LSTMCell(num_units=RNN_SIZE, num_proj=RNN_PROJ)
-		cell_with_ground_truth = SamplingRNNCell(num_outputs=OUTPUT_DIM, use_ground_truth=True,
-												 internal_cell=internal_cell)
-		cell_autoregressive = SamplingRNNCell(num_outputs=OUTPUT_DIM, use_ground_truth=False,
-											  internal_cell=internal_cell)
-
-
-	def get_initial_state(complex_state_tuple_sizes):
-		flat_sizes = nest.flatten(complex_state_tuple_sizes)
-		init_state_flat = [tf.tile(
-			multiples=[BATCH_SIZE, 1],
-			input=tf.get_variable("controller_initial_state_%d" % i, initializer=tf.zeros_initializer, shape=([1, s]),
-								  dtype=tf.float32))
-			for i, s in enumerate(flat_sizes)]
-		init_state = nest.pack_sequence_as(complex_state_tuple_sizes, init_state_flat)
-		return init_state
+			internal_cell = tf.nn.rnn_cell.LSTMCell(num_units=RNN_SIZE, num_proj=RNN_PROJ)
+			cell_with_ground_truth = SamplingRNNCell(num_outputs=OUTPUT_DIM, use_ground_truth=True,
+													 internal_cell=internal_cell)
+			cell_autoregressive = SamplingRNNCell(num_outputs=OUTPUT_DIM, use_ground_truth=False,
+												  internal_cell=internal_cell)
 
 
-	def deep_copy_initial_state(complex_state_tuple):
-		flat_state = nest.flatten(complex_state_tuple)
-		flat_copy = [tf.identity(s) for s in flat_state]
-		deep_copy = nest.pack_sequence_as(complex_state_tuple, flat_copy)
-		return deep_copy
+		def get_initial_state(complex_state_tuple_sizes):
+			flat_sizes = nest.flatten(complex_state_tuple_sizes)
+			init_state_flat = [tf.tile(
+				multiples=[BATCH_SIZE, 1],
+				input=tf.get_variable("controller_initial_state_%d" % i, initializer=tf.zeros_initializer,
+									  shape=([1, s]),
+									  dtype=tf.float32))
+				for i, s in enumerate(flat_sizes)]
+			init_state = nest.pack_sequence_as(complex_state_tuple_sizes, init_state_flat)
+			return init_state
 
 
-	with tf.device('/gpu:0'):
-		controller_initial_state_variables = get_initial_state(cell_autoregressive.state_size)
-		controller_initial_state_autoregressive = deep_copy_initial_state(controller_initial_state_variables)
-		controller_initial_state_gt = deep_copy_initial_state(controller_initial_state_variables)
+		def deep_copy_initial_state(complex_state_tuple):
+			flat_state = nest.flatten(complex_state_tuple)
+			flat_copy = [tf.identity(s) for s in flat_state]
+			deep_copy = nest.pack_sequence_as(complex_state_tuple, flat_copy)
+			return deep_copy
 
-	with tf.variable_scope("predictor"):
-		out_gt, controller_final_state_gt = tf.nn.dynamic_rnn(cell=cell_with_ground_truth,
-															  inputs=rnn_inputs_with_ground_truth,
-															  sequence_length=[SEQ_LEN] * BATCH_SIZE,
-															  initial_state=controller_initial_state_gt,
-															  dtype=tf.float32,
-															  swap_memory=True, time_major=False)
 
-	with tf.variable_scope("predictor", reuse=True):
-		out_autoregressive, controller_final_state_autoregressive = tf.nn.dynamic_rnn(cell=cell_autoregressive,
-																					  inputs=rnn_inputs_autoregressive,
-																					  sequence_length=[
-																										  SEQ_LEN] * BATCH_SIZE,
-																					  initial_state=controller_initial_state_autoregressive,
-																					  dtype=tf.float32,
-																					  swap_memory=True,
-																					  time_major=False)
+		with tf.device('/gpu:0'):
+			controller_initial_state_variables = get_initial_state(cell_autoregressive.state_size)
+			controller_initial_state_autoregressive = deep_copy_initial_state(controller_initial_state_variables)
+			controller_initial_state_gt = deep_copy_initial_state(controller_initial_state_variables)
 
-	with tf.device('/gpu:0'):
-		mse_gt = tf.reduce_mean(tf.squared_difference(out_gt, targets_normalized))
-		mse_autoregressive = tf.reduce_mean(tf.squared_difference(out_autoregressive, targets_normalized))
-		mse_autoregressive_steering = tf.reduce_mean(
-			tf.squared_difference(out_autoregressive[:, :, 0], targets_normalized[:, :, 0]))
-		steering_predictions = (out_autoregressive[:, :, 0] * contrib_dataset.std[0]) + contrib_dataset.mean[0]
+		with tf.variable_scope("predictor"):
+			out_gt, controller_final_state_gt = tf.nn.dynamic_rnn(cell=cell_with_ground_truth,
+																  inputs=rnn_inputs_with_ground_truth,
+																  sequence_length=[SEQ_LEN] * BATCH_SIZE,
+																  initial_state=controller_initial_state_gt,
+																  dtype=tf.float32,
+																  swap_memory=True, time_major=False)
 
-		total_loss = mse_autoregressive_steering + aux_cost_weight * (mse_gt + mse_autoregressive)
+		with tf.variable_scope("predictor", reuse=True):
+			out_autoregressive, controller_final_state_autoregressive = tf.nn.dynamic_rnn(cell=cell_autoregressive,
+																						  inputs=rnn_inputs_autoregressive,
+																						  sequence_length=[
+																											  SEQ_LEN] * BATCH_SIZE,
+																						  initial_state=controller_initial_state_autoregressive,
+																						  dtype=tf.float32,
+																						  swap_memory=True,
+																						  time_major=False)
 
-		optimizer = get_optimizer(total_loss, learning_rate)
+		with tf.device('/gpu:0'):
+			with tf.name_scope("regression"):
+				mse_gt = tf.reduce_mean(tf.squared_difference(out_gt, targets_normalized))
+				mse_autoregressive = tf.reduce_mean(tf.squared_difference(out_autoregressive, targets_normalized))
+				mse_autoregressive_steering = tf.reduce_mean(
+					tf.squared_difference(out_autoregressive[:, :, 0], targets_normalized[:, :, 0]))
+				steering_predictions = (out_autoregressive[:, :, 0] * contrib_dataset.std[0]) + contrib_dataset.mean[0]
 
-		mse_autoregressive_steering_sqrt = tf.sqrt(mse_autoregressive_steering)
-		mse_gt_sqrt = tf.sqrt(mse_gt)
-		mse_autoregressive_sqrt = tf.sqrt(mse_autoregressive)
+				total_loss = mse_autoregressive_steering + aux_cost_weight * (mse_gt + mse_autoregressive)
 
-	with tf.device('/cpu:0'):
-		tf.summary.scalar("MAIN TRAIN METRIC: rmse_autoregressive_steering", mse_autoregressive_steering_sqrt)
-		tf.summary.scalar("rmse_gt", mse_gt_sqrt)
-		tf.summary.scalar("rmse_autoregressive", mse_autoregressive_sqrt)
-		summaries = tf.summary.merge_all()
-		train_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/train_summary', graph=graph)
-		valid_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/valid_summary', graph=graph)
-		test_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/valid_summary', graph=graph)
-		saver = tf.train.Saver(write_version=tf.train.SaverDef.V2)
+				optimizer = get_optimizer(total_loss, learning_rate)
+
+				mse_autoregressive_steering_sqrt = tf.sqrt(mse_autoregressive_steering)
+				mse_gt_sqrt = tf.sqrt(mse_gt)
+				mse_autoregressive_sqrt = tf.sqrt(mse_autoregressive)
+
+		with tf.device('/cpu:0'):
+			tf.summary.scalar("MAIN TRAIN METRIC: rmse_autoregressive_steering", mse_autoregressive_steering_sqrt)
+			tf.summary.scalar("rmse_gt", mse_gt_sqrt)
+			tf.summary.scalar("rmse_autoregressive", mse_autoregressive_sqrt)
+			summaries = tf.summary.merge_all()
+			train_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/train_summary', graph=graph)
+			valid_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/valid_summary', graph=graph)
+			test_writer = tf.summary.FileWriter(CHECKPOINT_DIR + '/valid_summary', graph=graph)
+			saver = tf.train.Saver(write_version=tf.train.SaverDef.V2)
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
 
@@ -207,12 +213,7 @@ def do_epoch(sess, pipelines, type: DatasetType):
 
 	for step in range(batch_count):
 		print("Deque next batch")
-		batch = None
-		while batch is None:
-			if sess.run(pipeline.queue.size()) > 0:
-				batch = sess.run(pipeline.dequeue_op)
-			else:
-				print("pipeline empty!")
+		batch = sess.run(pipeline.dequeue_op)
 		feed_dict = {video: batch[0], targets_normalized: batch[1]}
 		if controller_final_state_autoregressive_cur is not None:
 			feed_dict.update({controller_initial_state_autoregressive: controller_final_state_autoregressive_cur})
@@ -278,20 +279,21 @@ with tf.Session(graph=graph, config=tf.ConfigProto(gpu_options=gpu_options,
 
 	with tf.Session() as sess:
 		with tf.device('/cpu:0'):
-			mean, std, batch_containers = contrib_dataset.get_datasets()
-			pipelineOptions = PipelineOptions()
-			# pipelines = [Pipeline(graph, sess, batch_container, pipelineOptions) for batch_container in batch_containers]
-			# for pipeline in pipelines:
-			# 	pipeline.start_pipeline()
-			pipeline = Pipeline(graph, sess, batch_containers[DatasetType.TRAIN.value], pipelineOptions)
-			pipeline.start_pipeline()
+			with tf.name_scope("pipeline"):
+				mean, std, batch_containers = contrib_dataset.get_datasets()
+				pipelineOptions = PipelineOptions()
+				# pipelines = [Pipeline(graph, sess, batch_container, pipelineOptions) for batch_container in batch_containers]
+				# for pipeline in pipelines:
+				# 	pipeline.start_pipeline()
+				pipeline = Pipeline(graph, sess, batch_containers[DatasetType.TRAIN.value], pipelineOptions)
+				pipeline.start_pipeline()
 
 		with tf.device('/gpu:0'):
 			for epoch in range(NUM_EPOCHS):
 				print("Starting epoch %d / %d" % (epoch, NUM_EPOCHS))
 
 				print("Validation:")
-				valid_score, valid_predictions = do_epoch(sess=session, pipelines=[pipeline],
+				valid_score, valid_predictions = do_epoch(sess=sess, pipelines=[pipeline],
 														  type=DatasetType.VALIDATION)
 
 				if best_validation_score is None:
@@ -309,12 +311,12 @@ with tf.Session(graph=graph, config=tf.ConfigProto(gpu_options=gpu_options,
 
 				if epoch != NUM_EPOCHS - 1:
 					print("Training")
-					do_epoch(sess=session, pipelines=[pipeline], type=DatasetType.TRAIN)
+					do_epoch(sess=sess, pipelines=[pipeline], type=DatasetType.TRAIN)
 
 				if epoch == NUM_EPOCHS - 1:
 					print("Test:")
 					with open(CHECKPOINT_DIR + "/test-predictions-epoch%d" % epoch, "w") as out:
-						_, test_predictions = do_epoch(sess=session, pipelines=[pipeline], type=DatasetType.TEST)
+						_, test_predictions = do_epoch(sess=sess, pipelines=[pipeline], type=DatasetType.TEST)
 						print("frame_id,steering_angle")
 						for img, pred in test_predictions.items():
 							img = img.replace("challenge_2/Test-final/center/", "")
